07f02cdcdcaab7cbd79f7ec17056a2741ff6fca3
regularPark <srk99279@naver.com>
2023-09-01 11:37:13 +0900

    Updates

--- bundle.js
+++ bundle.js
@@ -7406,8 +7406,8 @@ function createSearchParams(init) {
 // EXTERNAL MODULE: ./node_modules/classnames/bind.js
 var bind = __webpack_require__(7166);
 var bind_default = /*#__PURE__*/__webpack_require__.n(bind);
-;// CONCATENATED MODULE: ./src/assets/images/hero.webp
-/* harmony default export */ const hero = (__webpack_require__.p + "static/hero.webp");
+;// CONCATENATED MODULE: ./src/assets/images/hero.png
+/* harmony default export */ const hero = (__webpack_require__.p + "static/hero.png");
 ;// CONCATENATED MODULE: ./src/assets/images/trending.gif
 /* harmony default export */ const trending = (__webpack_require__.p + "static/trending.gif");
 ;// CONCATENATED MODULE: ./src/assets/images/find.gif
